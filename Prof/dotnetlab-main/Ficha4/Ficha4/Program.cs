cfa70e259a48c7ab5f5a192b253a7f421c47079e
Carlos Costa <andrefr1995@gmail.com>
2022-03-22 11:22:36 +0000

    prof

---
+++ Prof/dotnetlab-main/Ficha4/Ficha4/Program.cs
@@ -0,0 +1,62 @@
+﻿using Ficha4;
+
+Point p1 = new Point();
+Point p2 = new Point(0,10);
+Point p3 = new Point(10, 10);
+
+Console.WriteLine("x: " + p1.X);
+
+p1.X = 10;
+
+Console.WriteLine("x: " + p1.X);
+
+double distance = p1.DistanceTo(p2);
+Console.WriteLine("Distance: " + distance);
+
+Triangle t1 = new Triangle();
+Triangle t2 = new Triangle(p1, p2, p3);
+
+double baseT = t2.Base();
+double heightT = t2.Height();
+double area = t2.Area();
+
+Console.WriteLine("Base: " + baseT);
+Console.WriteLine("Height: " + heightT);
+Console.WriteLine("Area: " + area);
+
+Rectangle r1 = new Rectangle();
+Rectangle r2 = new Rectangle(new Point(0, 5), 5, 5);
+
+double areaRect = r2.Area();
+double perimRect = r2.Perimeter();
+
+Console.WriteLine("Area Rectangle: " + areaRect);
+
+Point point = new Point(1, 4);
+
+Point point2 = new Point(6, 6);
+
+Console.WriteLine(point.ToString());
+
+
+
+bool contains = r2.Contains(point);
+bool contains2 = r2.Contains(point2);
+
+Console.WriteLine(contains2);
+
+Circle c1 = new Circle();
+Circle c2 = new Circle(new Point(5, 5), 5);
+
+double areaC = c2.Area();
+double perimeterC = c2.Perimeter();
+
+Console.WriteLine("Area do circulo:" + areaC);
+Console.WriteLine("Posição do círculo:" + c2.Position);
+
+Console.WriteLine(c2.ToString());
+Console.WriteLine(c1.ToString());
+
+
+
+
---
+++ Prof/dotnetlab-main/Ficha4/Ficha4/Triangle.cs
@@ -0,0 +1,65 @@
+﻿using Ficha4;
+
+public class Triangle : Shape
+{
+    
+    private Point b;
+    private Point c;
+
+    public Triangle()
+    {
+        this.position = new Point();
+        this.b = new Point();
+        this.c = new Point();
+    }
+
+    public Triangle(Point a, Point b, Point c)
+    {
+        this.position = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public Point A
+    {
+        get { return position; }
+        set { position = value; }
+    }
+    public Point B
+    {
+        get { return b; }
+        set { b = value; }
+    }
+    public Point C
+    {
+        get { return c; }
+        set { c = value; }
+    }
+
+    public double Base()
+    {
+        return position.DistanceTo(b);
+    }
+
+    public double Height()
+    {
+        return position.DistanceTo(c);
+    }
+
+    public override double Area()
+    {
+        return (Base() * Height()) / 2;
+    }
+
+    public override double Perimeter()
+    {
+       /* double l1 = position.DistanceTo(b);
+        double l2 = position.DistanceTo(c);
+        double l3 = b.DistanceTo(c);
+
+        double perimeter = l1 + l2 + l3;
+        return perimeter;*/
+
+        return position.DistanceTo(b) + position.DistanceTo(c) + b.DistanceTo(c);
+    }
+}
